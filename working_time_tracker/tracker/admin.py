a632bcf12bd7c96ff3e5928ea2136c2b73d6cf2a
osha890 <oshavshav@gmail.com>
2025-05-06 23:34:21 +0300

    Added test model

--- working_time_tracker/tracker/admin.py
+++ working_time_tracker/tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TestExample
 
-# Register your models here.
+
+@admin.register(TestExample)
+class TestExampleAdmin(admin.ModelAdmin):
+    pass
