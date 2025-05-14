ea8f64b287b2d13fcbe25f0babd885a9ec2e4c62
osha890 <oshavshav@gmail.com>
2025-05-14 13:03:15 +0300

    Formatted all files according to pre-commit config

--- working_time_tracker/tracker/admin.py
+++ working_time_tracker/tracker/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.admin import ModelAdmin, register
+
 from .models import TestExample
 
 
--- working_time_tracker/working_time_tracker/asgi.py
+++ working_time_tracker/working_time_tracker/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'working_time_tracker.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "working_time_tracker.settings")
 
 application = get_asgi_application()
