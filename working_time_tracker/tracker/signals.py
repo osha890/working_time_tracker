2701838216d09e149d6480e46bc42395aeb943b3
osha890 <oshavshav@gmail.com>
2025-05-21 00:40:43 +0300

    Added signal for UserExtension

---
+++ working_time_tracker/tracker/signals.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import UserExtension
+
+
+@receiver(post_save, sender=User)
+def create_user_extension(sender, instance, created, **kwargs):
+    if created:
+        UserExtension.objects.create(user=instance)
