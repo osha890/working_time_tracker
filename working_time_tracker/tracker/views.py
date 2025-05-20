6800363867c297c7d336b053bafd5a092c839a46
osha890 <oshavshav@gmail.com>
2025-05-20 23:06:42 +0300

    Added ViewSets

--- working_time_tracker/tracker/views.py
+++ working_time_tracker/tracker/views.py
@@ -1,3 +1,29 @@
-# from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Project, Task, Track, UserExtension
+from .serializers import (
+    ProjectSerializer,
+    TaskSerializer,
+    TrackSerializer,
+    UserExtensionSerializer,
+)
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class UserExtensionViewSet(ModelViewSet):
+    queryset = UserExtension.objects.all()
+    serializer_class = UserExtensionSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TrackViewSet(ModelViewSet):
+    queryset = Track.objects.all()
+    serializer_class = TrackSerializer
