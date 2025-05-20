8036d57abf7924dcdd055fcbfa2bd5cfb9b2639e
osha890 <oshavshav@gmail.com>
2025-05-20 21:46:20 +0300

    Added __str__ to models

--- working_time_tracker/tracker/models.py
+++ working_time_tracker/tracker/models.py
@@ -6,12 +6,18 @@ class Project(models.Model):
     title = models.CharField(max_length=50)  # type: ignore
     description = models.TextField(null=True, blank=True)  # type: ignore
 
+    def __str__(self):
+        return self.title
+
 
 class UserExtension(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # type: ignore
     project = models.ForeignKey(  # type: ignore
         Project, on_delete=models.SET_NULL, null=True, blank=True, related_name="user_extensions"
     )
+
+    def __str__(self):
+        return self.user.username
 
 
 class Task(models.Model):
@@ -23,6 +29,9 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)  # type: ignore
     is_in_progress = models.BooleanField(default=False)  # type: ignore
     is_completed = models.BooleanField(default=False)  # type: ignore
+
+    def __str__(self):
+        return self.title
 
 
 class Track(models.Model):
