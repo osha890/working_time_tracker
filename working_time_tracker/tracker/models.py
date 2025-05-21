329f370612c09c5df397e543245ec9501c7de309
osha890 <oshavshav@gmail.com>
2025-05-21 13:06:53 +0300

    Removed 'type: ignore' from models

--- working_time_tracker/tracker/models.py
+++ working_time_tracker/tracker/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=50)  # type: ignore
-    description = models.TextField(null=True, blank=True)  # type: ignore
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class UserExtension(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # type: ignore
-    project = models.ForeignKey(  # type: ignore
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(
         Project, on_delete=models.SET_NULL, null=True, blank=True, related_name="user_extensions"
     )
 
@@ -21,22 +21,20 @@ class UserExtension(models.Model):
 
 
 class Task(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")  # type: ignore
-    user = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="tasks"  # type: ignore
-    )
-    title = models.CharField(max_length=50)  # type: ignore
-    description = models.TextField(null=True, blank=True)  # type: ignore
-    is_in_progress = models.BooleanField(default=False)  # type: ignore
-    is_completed = models.BooleanField(default=False)  # type: ignore
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="tasks")
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    is_in_progress = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
 
 class Track(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tracks")  # type: ignore
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="tracks")  # type: ignore
-    is_in_progress = models.BooleanField()  # type: ignore
-    time_from = models.DateTimeField()  # type: ignore
-    time_to = models.DateTimeField(null=True, blank=True)  # type: ignore
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tracks")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="tracks")
+    is_in_progress = models.BooleanField()
+    time_from = models.DateTimeField()
+    time_to = models.DateTimeField(null=True, blank=True)
