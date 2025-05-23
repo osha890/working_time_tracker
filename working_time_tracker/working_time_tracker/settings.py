390bf61b01979050abf494b8b8caa09c627e9dee
osha890 <oshavshav@gmail.com>
2025-05-23 22:27:18 +0300

    Set up CORS

--- working_time_tracker/working_time_tracker/settings.py
+++ working_time_tracker/working_time_tracker/settings.py
@@ -30,16 +30,22 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tracker.apps.TrackerConfig",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "working_time_tracker.urls"
