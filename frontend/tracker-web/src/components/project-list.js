c895031532c49d8dfc87d08965649055e22177de
osha890 <oshavshav@gmail.com>
2025-05-24 22:19:38 +0300

    Added button "Add project"

--- frontend/tracker-web/src/components/project-list.js
+++ frontend/tracker-web/src/components/project-list.js
@@ -2,15 +2,18 @@ import React from "react";
 
 function ProjectList(props) {
     return (
-        <div className="projectList">
-            {props.projects.map(project => {
-                return (
-                    <div className="projectCard" key={project.id}>
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                    </div>
-                )
-            })}
+        <div>
+            <div className="addProjectButton" onClick={props.onAddClick}>Add project</div>
+            <div className="projectList">
+                {props.projects.map(project => {
+                    return (
+                        <div className="projectCard" key={project.id}>
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
