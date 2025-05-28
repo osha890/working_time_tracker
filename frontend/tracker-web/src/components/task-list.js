b8f0d39b1846c3d0dc2f636f5c93b65c097994d2
osha890 <oshavshav@gmail.com>
2025-05-28 18:59:47 +0300

    Updated task card

--- frontend/tracker-web/src/components/task-list.js
+++ frontend/tracker-web/src/components/task-list.js
@@ -18,17 +18,21 @@ function TaskList(props) {
                                 <p>{task.description || "No description provided."}</p>
                                 <p><b>Project: {task.project.title}</b></p>
                             </div>
-
-                            {task.user && (
-                                <div>
-                                    <p>Assigned to: {task.user.username}</p>
-                                    {task.is_completed ? (
-                                        <div>Completed</div>
-                                    ) : task.is_in_progress ? (
-                                        <div>In progress</div>
-                                    ) : null}
-                                </div>
-                            )}
+                            <div>
+                                <p>Reported by: {task.reporter.username}</p>
+                                {task.assignee && (
+                                    <React.Fragment>
+                                        <p>Assigned to: {task.assignee.username}</p>
+                                        <div className="status">
+                                        {task.is_completed ? (
+                                            <p>Completed</p>
+                                        ) : task.is_in_progress ? (
+                                            <p>In progress</p>
+                                        ) : null}
+                                        </div>
+                                    </React.Fragment>
+                                )}
+                            </div>
                         </div>
                     )
                 })}
