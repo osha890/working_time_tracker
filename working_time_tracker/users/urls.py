bfa3845b0a933936a110674878fd70e69953c6f3
osha890 <oshavshav@gmail.com>
2025-05-28 20:49:05 +0300

    Added token urls

--- working_time_tracker/users/urls.py
+++ working_time_tracker/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import RegisterView
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
+    path("token/", TokenObtainPairView.as_view()),
+    path("token-refresh/", TokenRefreshView.as_view()),
 ]
