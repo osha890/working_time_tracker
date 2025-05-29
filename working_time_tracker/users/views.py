0fb36bc7c747ee9279186f7cbca2afd8c0834bbf
osha890 <oshavshav@gmail.com>
2025-05-29 16:43:11 +0300

    Updated import paths

--- working_time_tracker/users/views.py
+++ working_time_tracker/users/views.py
@@ -1,8 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from .serializers import RegisterSerializer
+from users.serializers import RegisterSerializer
 
 
 class RegisterView(APIView):
