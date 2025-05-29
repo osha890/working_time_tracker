5d9756915f90873dc6cacd8abb29a41ee8bc7e6d
osha890 <oshavshav@gmail.com>
2025-05-29 22:20:15 +0300

    Changed permissions for RegisterView

--- working_time_tracker/users/views.py
+++ working_time_tracker/users/views.py
@@ -1,10 +1,15 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.serializers import RegisterSerializer
 
 
 class RegisterView(APIView):
+    permission_classes = [
+        AllowAny,
+    ]
+
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
