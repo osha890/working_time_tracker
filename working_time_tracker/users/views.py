c44af5dafab4cbf27f70e62d69e17684e382c38e
osha890 <oshavshav@gmail.com>
2025-05-30 16:14:55 +0300

    Added raise_exception=True to serializer.is_valid()

--- working_time_tracker/users/views.py
+++ working_time_tracker/users/views.py
@@ -12,7 +12,6 @@ class RegisterView(APIView):
 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "User successfully created"}, status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message": "User successfully created"}, status.HTTP_201_CREATED)
