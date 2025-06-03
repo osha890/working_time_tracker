88072193e98a0c8af95ea3f95eaebbe063ee9ddf
osha890 <oshavshav@gmail.com>
2025-06-03 19:06:53 +0300

    Added created_ad fields to Project and Task

--- working_time_tracker/tracker/models.py
+++ working_time_tracker/tracker/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -41,6 +42,7 @@ class Task(models.Model):
         choices=TaskStatus.choices,
         default=TaskStatus.TO_DO,
     )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
