6a53f1e3f21f4eb50cef71df4d8f4ba2748a6da7
osha890 <oshavshav@gmail.com>
2025-06-05 19:21:44 +0300

    Added task taking endpoint

--- working_time_tracker/tracker/views/task.py
+++ working_time_tracker/tracker/views/task.py
@@ -1,3 +1,7 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from tracker.models import Task
 from tracker.serializers.task import (
     TaskDetailedSerializer,
@@ -14,3 +18,17 @@ class TaskViewSet(BaseModelViewSet):
         "list": TaskListSerializer,
         "retrieve": TaskDetailedSerializer,
     }
+
+    @action(detail=True, methods=["post"])
+    def take(self, request, pk=None):
+        task = self.get_object()
+
+        if task.assignee is not None:
+            return Response(
+                {"detail": "This task is already assigned to another user."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        task.assignee = request.user
+        task.save()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
