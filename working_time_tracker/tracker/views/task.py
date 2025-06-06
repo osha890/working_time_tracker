2247d3f85d17e31ded0f7de2e84a563a661fb716
osha890 <oshavshav@gmail.com>
2025-06-06 15:52:54 +0300

    Added status changing endpoint

--- working_time_tracker/tracker/views/task.py
+++ working_time_tracker/tracker/views/task.py
@@ -1,8 +1,10 @@
+from django.utils import timezone
+
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from tracker.models import Task, TaskStatus, UserExtension
+from tracker.models import Task, TaskStatus, Track, UserExtension
 from tracker.serializers.task import (
     TaskDetailedSerializer,
     TaskListSerializer,
@@ -48,6 +50,38 @@ class TaskViewSet(BaseModelViewSet):
         task.save()
         serializer = self.get_serializer(task)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"])
+    def change_status(self, request, pk=None):
+        task = self.get_object()
+
+        if task.assignee != request.user:
+            return Response({"detail": "You can not access this task"}, status=status.HTTP_403_FORBIDDEN)
+
+        new_status = request.data.get("status")
+        if new_status not in dict(TaskStatus.choices):
+            return Response({"detail": "Invalid status"}, status=status.HTTP_400_BAD_REQUEST)
+
+        now = timezone.now()
+
+        try:
+            active_track = task.tracks.get(user=request.user, time_to__isnull=True)
+            active_track.time_to = now
+            active_track.save()
+        except Track.DoesNotExist:
+            pass
+
+        Track.objects.create(
+            user=request.user,
+            task=task,
+            status=new_status,
+            time_from=now,
+        )
+
+        task.status = new_status
+        task.save()
+
+        return Response({"detail": "Status changed successfully"}, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=["get"])
     def accessible(self, request):
