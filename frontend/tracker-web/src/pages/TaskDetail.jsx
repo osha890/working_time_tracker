5d67ab9f6de5914f19e921761c9302acc85ddec9
osha890 <oshavshav@gmail.com>
2025-06-13 17:20:45 +0300

    Added links to TaskDetail

--- frontend/tracker-web/src/pages/TaskDetail.jsx
+++ frontend/tracker-web/src/pages/TaskDetail.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { fetchMyTasks } from '../utils/api';
+import { fetchAccessibleTasks, fetchTasks } from '../utils/api';
 import { Container, Typography, Box, Button } from '@mui/material';
 import TaskStatusSummary from '../components/TaskStatusSummary';
+import { useUser } from '../UserContext';
 
 function TaskDetail() {
     const { taskId } = useParams();
+    const { user } = useUser();
     const navigate = useNavigate();
     const [task, setTask] = useState(null);
 
     useEffect(() => {
         const loadTask = async () => {
             try {
-                const data = await fetchMyTasks();
+                let data;
+                if (user?.is_staff) {
+                    data = await fetchTasks();
+                } else {
+                    data = await fetchAccessibleTasks();
+                }
                 const foundTask = data.find(t => t.id === parseInt(taskId));
                 setTask(foundTask);
             } catch (error) {
@@ -20,7 +27,7 @@ function TaskDetail() {
             }
         };
         loadTask();
-    }, [taskId]);
+    }, [taskId, user]);
 
     if (!task) return <Typography>Loading...</Typography>;
 
@@ -37,6 +44,7 @@ function TaskDetail() {
             <Typography><strong>Project:</strong> {task.project?.title || '—'}</Typography>
             <Typography><strong>Description:</strong> {task.description || '—'}</Typography>
             <Typography><strong>Reporter:</strong> {task.reporter?.username || '—'}</Typography>
+            <Typography><strong>Assignee:</strong> {task.assignee?.username || '—'}</Typography>
             <Typography><strong>Created At:</strong> {new Date(task.created_at).toLocaleString('en-GB', {
                 year: 'numeric',
                 month: 'short',
@@ -44,10 +52,10 @@ function TaskDetail() {
                 hour: '2-digit',
                 minute: '2-digit'
             })}</Typography>
-
-            <Box mt={4}>
-                <TaskStatusSummary taskId={task.id} />
-            </Box>
+            {!user.is_staff ?
+                (<Box mt={4}>
+                    <TaskStatusSummary taskId={task.id} />
+                </Box>) : null}
         </Container>
     );
 }
