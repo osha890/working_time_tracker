3d215e7e8232ac2e12a0a593142abdfc76fc1e1a
osha890 <oshavshav@gmail.com>
2025-06-13 11:58:30 +0300

    Added projects ti xlsx report

--- working_time_tracker/tracker/services/reports.py
+++ working_time_tracker/tracker/services/reports.py
@@ -14,7 +14,7 @@ def generate_xlsx_report(report_data):
     wb = openpyxl.Workbook()
     ws = wb.active
 
-    headers = ["User ID", "Username", "Task ID", "Task Title", "Status", "Total Time"]
+    headers = ["Project ID", "Project Title", "User ID", "Username", "Task ID", "Task Title", "Status", "Total Time"]
     ws.append(headers)
 
     for col in ws.iter_cols(min_row=1, max_row=1, min_col=1, max_col=len(headers)):
@@ -22,15 +22,18 @@ def generate_xlsx_report(report_data):
             cell.font = Font(bold=True)
 
     for entry in report_data:
-        user_id = entry["user"]["id"]
-        username = entry["user"]["username"]
-        for task_info in entry["tasks"]:
-            task_id = task_info["task"]["id"]
-            task_title = task_info["task"]["title"]
-            for status_info in task_info["statuses"]:
-                status = status_info["status"]
-                total_time = status_info["total_time"]
-                ws.append([user_id, username, task_id, task_title, status, total_time])
+        project_id = entry["project"]["id"]
+        project_title = entry["project"]["title"]
+        for user_info in entry["users"]:
+            user_id = user_info["user"]["id"]
+            username = user_info["user"]["username"]
+            for task_info in user_info["tasks"]:
+                task_id = task_info["task"]["id"]
+                task_title = task_info["task"]["title"]
+                for status_info in task_info["statuses"]:
+                    status = status_info["status"]
+                    total_time = status_info["total_time"]
+                    ws.append([project_id, project_title, user_id, username, task_id, task_title, status, total_time])
 
     memory_object = BytesIO()
     wb.save(memory_object)
