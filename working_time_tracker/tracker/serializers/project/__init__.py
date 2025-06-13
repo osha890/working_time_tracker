dc3b9e1e21c22908b734598244b7efc8568ad34c
osha890 <oshavshav@gmail.com>
2025-06-13 18:55:34 +0300

    Avoided circular import

---
+++ working_time_tracker/tracker/serializers/project/__init__.py
@@ -0,0 +1,4 @@
+from .project_detailed_serializer import ProjectDetailedSerializer
+from .project_serializer import ProjectSerializer
+
+__all__ = ["ProjectSerializer", "ProjectDetailedSerializer"]
--- working_time_tracker/tracker/serializers/project/project_detailed_serializer.py
+++ working_time_tracker/tracker/serializers/project/project_detailed_serializer.py
@@ -4,15 +4,6 @@ from tracker.models import Project
 from tracker.serializers.task import TaskDetailedSerializer
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = "__all__"
-        read_only_fields = [
-            "id",
-        ]
-
-
 class ProjectDetailedSerializer(serializers.ModelSerializer):
     tasks = TaskDetailedSerializer(many=True, read_only=True)
 
