47ea497a266d25227eaf34d289c75857c343c7a5
osha890 <oshavshav@gmail.com>
2025-06-13 18:20:40 +0300

    Added my_project action

--- working_time_tracker/tracker/views/project.py
+++ working_time_tracker/tracker/views/project.py
@@ -1,3 +1,7 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from tracker.models import Project
 from tracker.serializers.project import ProjectSerializer
 from tracker.views.base import BaseModelViewSet
@@ -9,4 +13,16 @@ class ProjectViewSet(BaseModelViewSet):
         "default": ProjectSerializer,
         "list": ProjectSerializer,
         "retrieve": ProjectSerializer,
+        "my_project": ProjectSerializer,
     }
+
+    @action(detail=False, methods=["get"], url_path="my-project")
+    def my_project(self, request):
+        user = request.user
+
+        if not hasattr(user, "userextension") or not user.userextension.project:
+            return Response({"detail": "No project"}, status=status.HTTP_404_NOT_FOUND)
+
+        project = user.userextension.project
+        serializer = self.get_serializer(project)
+        return Response(serializer.data)
