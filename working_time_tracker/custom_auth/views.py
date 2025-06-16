92cf914b346c4611ac16241fc3a45eca6836fc59
osha890 <oshavshav@gmail.com>
2025-06-16 13:06:24 +0300

    Covered .blacklist() exceptions

--- working_time_tracker/custom_auth/views.py
+++ working_time_tracker/custom_auth/views.py
@@ -1,7 +1,9 @@
+from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from custom_auth.serializers import RegisterSerializer
@@ -33,5 +35,9 @@ class LogoutView(APIView):
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return Response({"detail": "No token provided"}, status=status.HTTP_400_BAD_REQUEST)
+        except TokenError as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except IntegrityError:
+            return Response({"detail": "Token is already in the blacklist"}, status=status.HTTP_400_BAD_REQUEST)
